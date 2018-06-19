503aab672332e8c50ee28db4a30688fbd8e39bb4
Hoi Yuen <hoikityuen@gmail.com>
2018-06-19 15:10:50 -0400

    feature-#4 show article list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import ArticleListContainer from './containers/ArticleListContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <ArticleListContainer
+            url="https://gist.githubusercontent.com/stefsic/6cccb57ca589369692aa850328d901cd/raw/f11e8fba16079af7d1c9c4a5f9eb66d6626f1217/test_api.json"
+            sortField="created"
+            sortOrder="DESC"
+          />
       </div>
     );
   }
