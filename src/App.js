1650fd2a75cfe1da42020ed69ba676d35c47b034
Hoi Yuen <hoikityuen@gmail.com>
2018-06-20 15:32:42 -0400

    feature-#6 setup redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
+
 import './App.css';
 
 import ArticleListContainer from './containers/ArticleListContainer'
 import moment from 'moment'
 
-class App extends Component {
-  render() {
+const App = (props) => {
+
+    const store = configureStore();
+
     const endpoint = {
       url: "https://gist.githubusercontent.com/stefsic/6cccb57ca589369692aa850328d901cd/raw/f11e8fba16079af7d1c9c4a5f9eb66d6626f1217/test_api.json",
       sortMap: new Map([
@@ -53,16 +58,17 @@ class App extends Component {
     return (
       <div className="App">
         <div className="article-list">
-          <ArticleListContainer
-            url={endpoint.url}
-            sortField="created"
-            sortOrder="DESC"
-            sortMap={endpoint.sortMap}
-          />
+          <Provider store={store}>
+            <ArticleListContainer
+              url={endpoint.url}
+              sortField="created"
+              sortOrder="DESC"
+              sortMap={endpoint.sortMap}
+            />
+          </Provider>
         </div>
       </div>
     );
-  }
 }
 
 export default App;
