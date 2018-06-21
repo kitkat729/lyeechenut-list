26387bf68d257b036d28c964a54c7cff1042f997
Hoi Yuen <hoikityuen@gmail.com>
2018-06-21 03:30:51 -0400

    feature-#6 added support for redux composer enhancer

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,11 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 export default function configureStore(initialState) {
+    const composeEnhancers = 
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+                // options like actionSanitizer, stateSanitizer
+            }) : compose;
+
+    const enhancer = composeEnhancers(
+        applyMiddleware(thunk)
+    );
+
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        enhancer
     );
 }
